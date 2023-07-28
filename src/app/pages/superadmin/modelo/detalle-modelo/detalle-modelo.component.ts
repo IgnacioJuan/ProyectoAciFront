df90088d7c87864218b9b3fb881868f7a0f13d89
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-07-27 23:23:51 -0500

    Modelo v3

--- src/app/pages/superadmin/modelo/detalle-modelo/detalle-modelo.component.ts
+++ src/app/pages/superadmin/modelo/detalle-modelo/detalle-modelo.component.ts
@@ -154,7 +154,7 @@ export class DetalleModeloComponent implements OnInit {
   ponderacionCriterio(event: Event, element: any) {
     event.stopPropagation();
     // código del método del botón
-    this.router.navigate(['/sup/ponderacion/ponderacion-criterio'], { queryParams: { criterio: element.id_criterio, modelo: this.id } });
+    this.router.navigate(['/sup/ponderacion/ponderacion-criterio'], { state: { criterio: element, modelo: this.model } });
   }
 
   mostrar(element: any) {
